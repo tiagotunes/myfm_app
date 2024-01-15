d9818424cf0afa7b1c80efc5d2614031d4b9f046
Tiago Antunes <tiago.antunes@glintt.com>
2024-01-15 18:02:06 +0000

    Add theme and color files

---
+++ app/lib/configs/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFF191919);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+  colors: [Color(0xFFFF501E), Color(0xFFFFA528)],
+);
---
+++ app/lib/configs/theme.dart
@@ -0,0 +1,8 @@
+import 'package:app/configs/colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: kBackgroundColor,
+  );
+}
