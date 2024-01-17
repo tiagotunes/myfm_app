3c829bdff2d0e67170d4546e5c8f0567a4c11b47
Tiago Antunes <tiago.antunes@glintt.com>
2024-01-17 12:28:05 +0000

    Setup Firebase

--- myfm/lib/main.dart
+++ myfm/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:myfm/configs/size.dart';
 import 'package:myfm/configs/theme.dart';
 import 'package:myfm/screens/welcome/welcome_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
