e1c8d5d363f762a4b659430f92342f8b76ef1c2c
Tiago Antunes <tiago.antunes@glintt.com>
2024-02-18 13:06:03 +0000

    Learn Custom Shapes

---
+++ myfm/lib/common/widgets/custom_shapes/containers/circular_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:myfm/utils/constants/colors.dart';
+
+class TCircularContainer extends StatelessWidget {
+  const TCircularContainer({
+    super.key,
+    this.width = 400,
+    this.height = 400,
+    this.radius = 400,
+    this.padding = 0,
+    this.child,
+    this.backgroundColor = TColors.white,
+  });
+
+  final double? width, height;
+  final double radius, padding;
+  final Widget? child;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: backgroundColor!,
+      ),
+      child: child,
+    );
+  }
+}
---
+++ myfm/lib/common/widgets/custom_shapes/curved_edges/curved_edges.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class TCustomCurvedEdges extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+
+    final firstCurve = Offset(0, size.height - 20);
+    final lastCurve = Offset(30, size.height - 20);
+    path.quadraticBezierTo(
+      firstCurve.dx,
+      firstCurve.dy,
+      lastCurve.dx,
+      lastCurve.dy,
+    );
+
+    final secondaryFirstCurve = Offset(0, size.height - 20);
+    final secondaryLastCurve = Offset(size.width - 30, size.height - 20);
+    path.quadraticBezierTo(
+      secondaryFirstCurve.dx,
+      secondaryFirstCurve.dy,
+      secondaryLastCurve.dx,
+      secondaryLastCurve.dy,
+    );
+
+    final thirdFirstCurve = Offset(size.width, size.height - 20);
+    final thirdLastCurve = Offset(size.width, size.height);
+    path.quadraticBezierTo(
+      thirdFirstCurve.dx,
+      thirdFirstCurve.dy,
+      thirdLastCurve.dx,
+      thirdLastCurve.dy,
+    );
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
