6e3374e348b5e0e7cefcde933e6b2d541825dade
Tiago Antunes <tiago.antunes@glintt.com>
2024-02-29 16:12:20 +0000

    Create Search Bar

--- myfm/lib/features/fm/screens/home/home.dart
+++ myfm/lib/features/fm/screens/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myfm/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:myfm/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:myfm/features/fm/screens/home/widgets/home_appbar.dart';
+import 'package:myfm/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,7 +15,15 @@ class HomeScreen extends StatelessWidget {
           children: [
             TPrimaryHeaderContainer(
               child: Column(
-                children: [THomeAppBar()],
+                children: [
+                  // Appbar
+                  THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  // Search bar
+                  TSearchContainer(text: "Search"),
+                  SizedBox(height: TSizes.spaceBtwSections * 1.5),
+                ],
               ),
             ),
           ],
