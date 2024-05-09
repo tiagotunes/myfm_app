82c32d73f9ccca7ebef65d370f8eb8aed8c18854
Tiago Antunes <tiago.antunes@glintt.com>
2024-05-09 12:50:21 +0100

    Setup Firebase

--- myfm/lib/main.dart
+++ myfm/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:myfm/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:myfm/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
+  // Load all the Material Design, Themes, Localizations, Bindings
   runApp(const App());
 }
