2b24f5cf8a969153f6a3886f4dd0cc8329eb4774
Tiago Antunes <tiago.antunes@glintt.com>
2024-05-15 18:24:48 +0100

    Uncommet exceptions

--- myfm/lib/data/repositories/user/user_repository.dart
+++ myfm/lib/data/repositories/user/user_repository.dart
@@ -1,6 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:myfm/features/personalization/models/user_model.dart';
+import 'package:myfm/utils/exceptions/firebase_auth_exceptions.dart';
+import 'package:myfm/utils/exceptions/firebase_exceptions.dart';
+import 'package:myfm/utils/exceptions/format_exceptions.dart';
+import 'package:myfm/utils/exceptions/platform_exceptions.dart';
 
 class UserRepository extends GetxController {
   static UserRepository get instance => Get.find();
@@ -11,15 +17,15 @@ class UserRepository extends GetxController {
   Future<void> saveUserRecord(UserModel user) async {
     try {
       await _db.collection("User").doc(user.id).set(user.toJson());
-    } /*on FirebaseAuthException catch (e) {
-      throw '';
+    } on FirebaseAuthException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
     } on FirebaseException catch (e) {
-      throw '';
-    } on FormatException catch (e) {
-      throw '';
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw TFormatException();
     } on PlatformException catch (e) {
-    }*/
-    catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
       throw 'Something went wrong. Please try again';
     }
   }
